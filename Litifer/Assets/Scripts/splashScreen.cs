f428deda02421ff90edf897e7b6e854179c96e6b
prathyusha-manu <68025187+prathyusha-manu@users.noreply.github.com>
2020-07-08 23:30:03 +0530

    Add Empty Project

---
+++ Litifer/Assets/Scripts/splashScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class splashScreen : MonoBehaviour
+{
+
+    void Start()
+    {
+       
+         //   Invoke("change", 3f);
+    }
+
+    public void change()
+    {
+        SceneManager.LoadScene("scrollview");
+    }
+    public void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                change();
+            }
+        }
+
+
+
+        if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    Application.Quit();
+                }
+            }
+    }
+}
